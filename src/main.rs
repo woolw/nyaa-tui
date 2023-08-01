a835bd1ccf6e3fb1ce2f551f5ded4719f32467d4
woolw <w.wagenhaeuser@gmail.com>
2023-08-01 20:33:41 +0200

    implemented base scrape

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 mod nyaa_scraper;
-fn main() {
-    nyaa_scraper::nyaa_scraper::get_html();
+
+#[tokio::main]
+async fn main() {
+    let mut entries = nyaa_scraper::get_list_entries().await;
+
+    entries.sort_by_key(|x| x.seeder);
+
+    for entry in entries.iter() {
+        println!("{:#?}", entry);
+    }
 }
--- src/nyaa_scraper.rs
+++ src/nyaa_scraper.rs
@@ -1,39 +1,64 @@
-pub mod nyaa_scraper {
-    // #[macro_use]
-    // extern crate unhtml;
-    // extern crate unhtml_derive;
-    // use unhtml_derive::{self, FromHtml};
-    //
-    // #[derive(FromHtml)]
-    // #[html(selector = ".default")]
-    // struct ListEntry {
-    //     #[html(selector = ".category-icon", attr = "alt")]
-    //     category: String,
-    //
-    //     name: String,
-    //     link: (String, String),
-    //     size: String,
-    //     date: String,
-    //     seeder: u32,
-    //     leecher: u32,
-    //     downloads: u32,
-    // }
+use unhtml::FromHtml;
 
-    pub fn get_html() {
-        // TODO fetch data from nyaa.si
-        // add filter and search functionality later on with enum
+// has to be done with nested structs, bcs topLevel Vec doesn't work
+#[derive(FromHtml, Debug)]
+struct TBody {
+    #[html(selector = ".default")]
+    html_entries: Vec<ListEntry>,
+}
 
-        let html = reqwest::blocking::get("https://nyaa.si/").unwrap().text();
+#[derive(FromHtml, Debug)]
+pub struct ListEntry {
+    #[html(selector = ".category-icon", attr = "alt")]
+    category: String,
+    #[html(selector = "td:nth-child(2)", attr = "inner")]
+    name: String,
+    #[html(selector = "td:nth-child(3)")]
+    download_links: DownloadLinks,
+    #[html(selector = "td:nth-child(4)", attr = "inner")]
+    size: String,
+    #[html(selector = "td:nth-child(5)", attr = "inner")]
+    date: String,
+    #[html(selector = "td:nth-child(6)", attr = "inner")]
+    pub seeder: u32,
+    #[html(selector = "td:nth-child(7)", attr = "inner")]
+    leecher: u32,
+    #[html(selector = "td:nth-child(8)", attr = "inner")]
+    downloads: u32,
+}
 
-        println!("{:#?}", html);
+#[derive(FromHtml, Debug)]
+pub struct DownloadLinks {
+    #[html(selector = "a:nth-child(1)", attr = "href")]
+    torrent: String,
+    #[html(selector = "a:nth-child(2)", attr = "href")]
+    magnetic: String,
+}
+
+async fn get_html() -> String {
+    let response = reqwest::get("https://nyaa.si/").await;
+
+    let mut html: String = "".to_string();
+    match response {
+        Ok(it) => html = it.text().await.unwrap(),
+        Err(err) => println!("{:#?}", err),
+    };
+
+    //println!("{:#?}", html);
+
+    html
+}
+
+pub async fn get_list_entries() -> Vec<ListEntry> {
+    let html = get_html().await;
+
+    let t_body = TBody::from_html(&html);
+
+    match t_body {
+        Ok(res) => {
+            println!("{:#?}", res);
+            res.html_entries
+        }
+        Err(err) => panic!("t_bodies were not OK {:#?}", err),
     }
-
-    // pub fn get_list_entries() -> Vec<ListEntry> {
-    // let html = get_html();
-    // let listEntries = Vec::<ListEntry>::from_html(html);
-    // match listEntries {
-    // Ok(expr) => expr,
-    // _ => panic!("listEntries were not OK"),
-    // }
-    // }
 }

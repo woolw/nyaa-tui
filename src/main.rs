660f76aa0d5d01b5ac43edc458dfe2594302423b
woolw <w.wagenhaeuser@gmail.com>
2023-08-08 11:01:10 +0200

    - moved datamodels - renamings - better demo_logging - retries on failed request - query params implementation - adjusted readme

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod nyaa_scraper;
 
 #[tokio::main]
 async fn main() {
-    let body = nyaa_scraper::get_body().await;
+    let body = nyaa_scraper::extract_body(None).await;
 
     println!("{:#?}", body);
 }
--- src/nyaa_scraper.rs
+++ src/nyaa_scraper.rs
@@ -1,93 +1,68 @@
+use self::datamodel::{Body, QueryParameters};
+use reqwest_middleware::ClientBuilder;
+use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
 use std::{fs::File, io::Write};
-
 use unhtml::FromHtml;
 
-#[derive(FromHtml, Debug)]
-pub struct Body {
-    #[html(selector = "select[name = f]:nth-child(1) > option")]
-    pub filter: Vec<Dropdowns>,
-    #[html(selector = "select[name = c]:nth-child(1) > option")]
-    pub categories: Vec<Dropdowns>,
-    #[html(selector = ".default")]
-    pub entries: Vec<ListEntry>,
-    #[html(selector = ".pagination")]
-    pub page_info: PageInfo,
-}
+pub mod datamodel;
 
-#[derive(FromHtml, Debug)]
-pub struct Dropdowns {
-    #[html(attr = "value")]
-    pub value: String,
-    #[html(attr = "title")]
-    pub title: String,
-}
+const BASE_URL: &str = "https://nyaa.si/";
 
-#[derive(FromHtml, Debug)]
-pub struct ListEntry {
-    #[html(selector = ".category-icon", attr = "alt")]
-    pub category: String,
-    #[html(selector = "td:nth-child(2)", attr = "inner")]
-    pub name: String,
-    #[html(selector = "td:nth-child(3)")]
-    pub download_links: DownloadLinks,
-    #[html(selector = "td:nth-child(4)", attr = "inner")]
-    pub size: String,
-    #[html(selector = "td:nth-child(5)", attr = "inner")]
-    pub date: String,
-    #[html(selector = "td:nth-child(6)", attr = "inner")]
-    pub seeder: u32,
-    #[html(selector = "td:nth-child(7)", attr = "inner")]
-    pub leecher: u32,
-    #[html(selector = "td:nth-child(8)", attr = "inner")]
-    pub downloads: u32,
-}
+pub async fn extract_body(params: Option<QueryParameters>) -> Body {
+    let html = get_response(params).await;
 
-#[derive(FromHtml, Debug)]
-pub struct DownloadLinks {
-    #[html(selector = "a:nth-child(1)", attr = "href")]
-    pub torrent: String,
-    #[html(selector = "a:nth-child(2)", attr = "href")]
-    pub magnetic: String,
-}
-
-#[derive(FromHtml, Debug)]
-pub struct PageInfo {
-    #[html(selector = "li:first-child > a", attr = "href")]
-    pub previous: Option<String>,
-    // has to be string, since sometimes the inner includes "(current)" inside a nested span
-    #[html(selector = ".active > a", attr = "inner")]
-    pub active: String,
-    #[html(selector = "li:last-child > a", attr = "href")]
-    pub next: Option<String>,
-}
-
-async fn get_html() -> String {
-    let response = reqwest::get("https://nyaa.si/?f=0&c=0_0&q=made&p=14").await;
-
-    let mut html = "".to_string();
-    match response {
-        Ok(it) => {
-            html = it.text().await.unwrap();
-            create_demo_files("demo.html", format!("{html:#}"));
-        }
-        Err(err) => println!("{:#?}", err),
-    };
-
-    html
-}
-
-pub async fn get_body() -> Body {
-    let html = get_html().await;
-
-    let t_body = Body::from_html(&html);
+    let t_body = Body::from_html(&html.unwrap_or("".to_string()));
 
     match t_body {
         Ok(res) => {
             create_demo_files("result.json", format!("{res:?}"));
             res
         }
-        Err(err) => panic!("t_bodies were not OK {:#?}", err),
+        Err(err) => panic!("the extracted Body was not OK {err:#?}"),
     }
+}
+
+async fn get_response(params: Option<QueryParameters>) -> Result<String, &'static str> {
+    let query_url = get_url(params);
+
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
+    let client = ClientBuilder::new(reqwest::Client::new())
+        .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+        .build();
+
+    let response = client.get(&query_url).send().await;
+
+    let mut html = "".to_string();
+    match response {
+        Ok(it) => {
+            html = it.text().await.unwrap_or("".to_string());
+            create_demo_files("demo.html", format!("{html:#}"));
+        }
+        Err(err) => {
+            create_demo_files("log.txt", format!("{} \n {}", err, query_url));
+        }
+    };
+
+    Ok(html)
+}
+
+fn get_url(params: Option<QueryParameters>) -> String {
+    let mut query_url: String = BASE_URL.to_string();
+
+    match params {
+        Some(ps) => {
+            query_url.push_str(
+                format!(
+                    "?f={}&c={}&q={}&p={}",
+                    ps.filter.value, ps.category.value, ps.search_query, ps.page
+                )
+                .as_str(),
+            );
+        }
+        None => {}
+    }
+
+    query_url
 }
 
 // soley for debugging

c52daeae803fa72f68e0295b8dd56e85ce55a3fb
woolw <w.wagenhaeuser@gmail.com>
2023-08-11 11:16:47 +0200

    playing around with rataTUI

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,54 @@
-use std::env;
+use std::{env, io, thread, time::Duration};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    prelude::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
 
 mod nyaa_scraper;
 
 #[tokio::main]
-async fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-    let write_demo_files: bool = args.iter().any(|x| x == "demo");
-    let body = nyaa_scraper::extract_body(None, write_demo_files).await;
+async fn main() -> Result<(), io::Error> {
+    // let args: Vec<String> = env::args().collect();
+    // dbg!(&args);
+    // let write_demo_files: bool = args.iter().any(|x| x == "demo");
+    // let body = nyaa_scraper::extract_body(None, write_demo_files).await;
+
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("nyaa-tui").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    // Start a thread to discard any input events. Without handling events, the
+    // stdin buffer will fill up, and be read into the shell when the program exits.
+    thread::spawn(|| loop {
+        event::read();
+    });
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }

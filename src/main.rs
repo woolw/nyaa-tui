134230fa2bd3b083572d5ba75e9ee9b1d5176eb9
woolw <w.wagenhaeuser@gmail.com>
2023-08-11 10:57:09 +0200

    allowed demo arg

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
+
 mod nyaa_scraper;
 
 #[tokio::main]
 async fn main() {
-    let body = nyaa_scraper::extract_body(None).await;
-
-    println!("{:#?}", body);
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let write_demo_files: bool = args.iter().any(|x| x == "demo");
+    let body = nyaa_scraper::extract_body(None, write_demo_files).await;
 }

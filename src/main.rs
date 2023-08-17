89930701f4aa8c7e8ec02aaf8212de24ae871317
woolw <w.wagenhaeuser@gmail.com>
2023-08-17 15:06:54 +0200

    clean

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<(), io::Error> {
     // args handler
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
-    let write_demo_files: bool = args.iter().any(|x| x == "demo");
+    let write_demo_files = args.iter().any(|x| x == "demo");
 
     // initial load of data
     let body = nyaa_scraper::extract_body(None, write_demo_files).await;
